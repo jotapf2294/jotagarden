03d87dc460b7eb0be0930c012b820f5009836730
jotapf2294 <jotapft22@gmail.com>
2026-04-20 00:54:33 +0100

    Atualizar o db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const db = new Dexie('JotaGardenDB');
 db.version(1).stores({
     zonas: '++id, nome',
-    wiki: '++id, especie, tempo, temp, info',
+    wiki: '++id, especie, categoria, tempo, temp, info',
     plantas: '++id, variedade, zonaId, data, nota',
     book: '++id, titulo, categoria, conteudo',
     config: 'id, val'
