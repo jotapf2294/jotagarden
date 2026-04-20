03e622ead83a632fa03b84335da454e1f35d08df
jotapf2294 <jotapft22@gmail.com>
2026-04-20 22:54:36 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,3 @@
-const CACHE_NAME = 'jotagarden-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  'https://unpkg.com/dexie/dist/dexie.js'
-];
-
-// Instalação: Cacheia os ficheiros base
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-// Ativação: Limpa caches antigas
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
-  );
-});
-
-// Estratégia: Tenta Rede, se falhar vai ao Cache
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+const CACHE = 'jotagarden-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/index.html','/style.css','/app.js']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
