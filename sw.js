d57bc9765185d915dd417ea2d95be706cb0c1c08
jotapf2294 <jotapft22@gmail.com>
2026-04-20 02:14:00 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE_NAME = 'jotagarden-v12';
-const ASSETS = [
-  './',
-  './index.html',
-  './db.js',
-  './manifest.json'
-];
+const cacheName = 'jotagarden-v1';
+const assets = ['./', './index.html', './db.js'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
