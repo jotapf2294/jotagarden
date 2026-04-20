ffc232c0ce47ceedadfa8a3da02e7423a6a0aee0
jotapf2294 <jotapft22@gmail.com>
2026-04-20 22:12:55 +0100

    Excluir db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const db = new Dexie("JotaGardenDB");
-db.version(1).stores({
-    plantas: '++id, variedade, especie, zonaId, data, ultimaRega, conteudo',
-    wiki: '++id, especie, categoria, tempo, temp, info',
-    zones: '++id, nome',
-    book: '++id, texto, data',
-    config: 'id, valor'
-});
