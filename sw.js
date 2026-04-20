dea07748f004018a5f3ddd4b3d73c5f5926dd88c
jotapf2294 <jotapft22@gmail.com>
2026-04-20 22:13:22 +0100

    Excluir sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE = 'jg-v4';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
