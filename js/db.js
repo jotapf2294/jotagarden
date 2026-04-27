2e7b3586db22bfc2129dde9b244d7ce345519f7a
jotapf2294 <jotapft22@gmail.com>
2026-04-28 00:07:59 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,38 +1,31 @@
 const DB = {
   db: null,
-  
   async init() {
     return new Promise((res, rej) => {
-      const req = indexedDB.open('BabeBakery', 1);
+      const req = indexedDB.open('BabeBakeryV21', 1);
       req.onupgradeneeded = e => {
         const db = e.target.result;
         if (!db.objectStoreNames.contains('receitas')) db.createObjectStore('receitas', {keyPath: 'id'});
         if (!db.objectStoreNames.contains('stock')) db.createObjectStore('stock', {keyPath: 'id'});
         if (!db.objectStoreNames.contains('encomendas')) db.createObjectStore('encomendas', {keyPath: 'id'});
+        if (!db.objectStoreNames.contains('config')) db.createObjectStore('config', {keyPath: 'key'});
       };
       req.onsuccess = e => { this.db = e.target.result; res(); };
       req.onerror = rej;
     });
   },
-
   async getAll(store) {
     const tx = this.db.transaction(store, 'readonly');
-    return new Promise(res => {
-      tx.objectStore(store).getAll().onsuccess = e => res(e.target.result);
-    });
+    return new Promise(res => tx.objectStore(store).getAll().onsuccess = e => res(e.target.result));
   },
-
   async save(store, data) {
     const tx = this.db.transaction(store, 'readwrite');
     if (!data.id) data.id = Date.now();
     tx.objectStore(store).put(data);
     return data.id;
   },
-
   async delete(store, id) {
     const tx = this.db.transaction(store, 'readwrite');
     tx.objectStore(store).delete(id);
   }
 };
-
-DB.init();
