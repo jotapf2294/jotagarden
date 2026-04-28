68c9d07d7ae49847da2848751729ba7ccc062daa
jotapf2294 <jotapft22@gmail.com>
2026-04-28 01:00:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,29 @@
-const CACHE = 'babe-v21';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './css/style.css'])));
-  self.skipWaiting();
+const CACHE_NAME = 'doce-gestao-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/db.js',
+  './js/modules/dashboard.js',
+  './js/modules/receitas.js',
+  './js/modules/agenda.js',
+  './js/modules/gestao.js'
+];
+
+// Instalação e Cache inicial
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+
+// Interceptar pedidos: Serve da Cache, se não tiver, vai à rede
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+}
+                     );
