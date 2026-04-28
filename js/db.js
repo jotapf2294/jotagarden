9bf9d0acf8b6cb118523419d271f3a894d61c9fd
jotapf2294 <jotapft22@gmail.com>
2026-04-28 16:27:09 +0100

    Atualizar o db.js

--- js/db.js
+++ js/db.js
@@ -2,9 +2,11 @@ import { openDB } from 'https://cdn.jsdelivr.net/npm/idb@8/+esm';
 
 const DB_NAME = 'DoceGestaoDB';
 const DB_VERSION = 1;
+let _db;
 
 export async function initDB() {
-  return openDB(DB_NAME, DB_VERSION, {
+  if (_db) return _db;
+  _db = await openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
       if (!db.objectStoreNames.contains('receitas')) {
         const s = db.createObjectStore('receitas', { keyPath: 'id' });
@@ -17,11 +19,18 @@ export async function initDB() {
       }
     }
   });
+  return _db;
 }
 
 export async function addData(store, data) {
   const db = await initDB();
   return db.put(store, data);
+}
+
+export async function addMultiple(store, items) {
+  const db = await initDB();
+  const tx = db.transaction(store, 'readwrite');
+  await Promise.all([...items.map(item => tx.store.put(item)), tx.done]);
 }
 
 export async function getAllData(store) {
