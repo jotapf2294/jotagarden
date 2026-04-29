29dda7f3f366e622986e25b4361e132cfdcd5ba1
jotapf2294 <jotapft22@gmail.com>
2026-04-29 17:00:33 +0100

    Excluir constants.js

--- js/modules/receitas/constants.js
+++
@@ -1,3 +0,0 @@
-export const ALERGENOS = ['Glúten','Ovos','Leite','Frutos Casca Rija','Soja','Amendoins','Sésamo','Peixe','Crustáceos','Mostarda','Aipo','Tremoço','Sulfitos','Moluscos'];
-export const CATEGORIAS = ['Bolos Aniversário','Pastelaria','Sobremesas','Padaria','Salgados','Sem Glúten','Vegan'];
-export const TIPOS_PCC = ['Biológico','Químico','Físico','Alergénico'];
